1b48f4168675f58613a6d2c548b5a31b02a2681a
CrossyChainsaw <74303221+CrossyChainsaw@users.noreply.github.com>
2024-11-27 15:39:02 +0100

    Load domain from .env

--- app/__init__.py
+++ app/__init__.py
@@ -3,13 +3,17 @@ from flask import Flask
 from flask_cors import CORS  # Import CORS to handle Cross-Origin Resource Sharing
 from app.routes import main  # Import the main Blueprint
 from app.endpoints import api  # Import the API Blueprint
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object('config')  # Load configurations from config.py
+    app.config.from_object('config')
 
     # Enable CORS for all routes (you can restrict to specific origins if needed)
-    CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
+    CORS(app, resources={r"/api/*": {"origins": os.environ.get("FRONTEND_DOMAIN")}})
     # CORS(app)
 
     # Register Blueprints
