e02022d5305af8abe040d0438b1b85d40584e004
CrossyChainsaw <74303221+CrossyChainsaw@users.noreply.github.com>
2024-12-24 13:14:45 +0100

    remove hardcoded host

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,13 @@
 # run.py
 from app import create_app
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = create_app()
 
+APP_HOST = os.environ.get()
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host=APP_HOST, port=5000)
